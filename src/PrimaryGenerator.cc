7f070cbf83f818864e371aa560905851e11ad724
Judith <judith@DESKTOP-QNNUPLD.>
2025-05-18 20:04:46 +0200

    Add muon generation

--- src/PrimaryGenerator.cc
+++ src/PrimaryGenerator.cc
@@ -1,33 +1,24 @@
 #include "PrimaryGenerator.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Event.hh"
+#include "Randomize.hh"
+#include <cmath>
+#include "G4PhysicalConstants.hh"
 
 PrimaryGenerator::PrimaryGenerator(){
 
     fParticleGun = new G4ParticleGun(1);
 
-    // Particle position
-
-    G4double x = 0. * m;
-    G4double y = 0. * m;
-    G4double z = 0. * m;
-
-    G4ThreeVector pos(x, y, z);
-
-    // Particle direction
-
-    G4double px = 0. ; 
-    G4double py = 0. ; 
-    G4double pz = 1. ;  // we make sure the particle is forwared in z direction
-
-    G4ThreeVector mom(px,py,pz);
 
     // Particle type
 
     G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
     G4ParticleDefinition *particle = particleTable -> FindParticle("mu-");
 
-    fParticleGun -> SetParticlePosition(pos);
-    fParticleGun -> SetParticleMomentumDirection(mom);
-    fParticleGun -> SetParticleEnergy(4. * MeV);
+    fParticleGun -> SetParticleEnergy(4. * GeV);
     fParticleGun -> SetParticleDefinition(particle);
 
 }
@@ -39,6 +30,31 @@ PrimaryGenerator::~PrimaryGenerator(){
 }
 
 void PrimaryGenerator::GeneratePrimaries(G4Event *anEvent){
+
+    // Particle position
+
+    G4double radius = 2.0 * m;
+    G4double r = radius * std::sqrt(G4UniformRand());
+    G4double phi1 = 2. * pi * G4UniformRand();
+    
+    G4double x = r * std::cos(phi1);
+    G4double y = r * std::sin(phi1);
+    G4double z = 1.5 * m;
+
+    G4ThreeVector pos(x, y, z);
+    fParticleGun->SetParticlePosition(pos);
+
+    // Particle direction
+    
+    G4double theta = std::acos(G4UniformRand());
+    G4double phi2 = 2. * pi * G4UniformRand();
+
+    G4double px = std::sin(theta) * std::cos(phi2); 
+    G4double py = std::sin(theta) * std::sin(phi2); 
+    G4double pz = -std::cos(theta);  // we make sure the particle is forwared in z negative direction
+
+    G4ThreeVector mom(px,py,pz);
+    fParticleGun->SetParticleMomentumDirection(mom);
 
     // Create vertex
 
